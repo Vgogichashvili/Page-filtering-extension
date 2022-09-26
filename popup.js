a96b3dc0e84ba6e712ec9a9469d6be75709f9706
vgogichashvili47@gmail.com <98263919+Vgogichashvili@users.noreply.github.com>
2022-09-26 14:28:00 +0400

    last

---
+++ popup.js
@@ -0,0 +1,24 @@
+// function autoRefresh() {
+//     if (location.href.indexOf('reload') == -1) {
+//         location.href = location.href + '?reload';
+//     }
+// }
+
+// autoRefresh()
+
+// const btnStart = document.getElementById("click-start");
+// const btnStop = document.getElementById("click-stop");
+
+
+
+
+// document.addEventListener("DOMContentLoaded",function(){
+//     btnStart.addEventListener("click",function(){
+//         var body = document.getElementsByTagName("body")[0].style.backgroundColor = "red"
+
+//     })
+//      btnStop.addEventListener("click",function(){
+//         var body = document.getElementsByTagName("body")[0].style.backgroundColor = ""
+
+//     })
+// })
---
+++ script.js
@@ -0,0 +1,59 @@
+function autoRefresh() {
+    if (location.href.indexOf('refreshed') == -1) {
+        location.href = location.href + '?refreshed';
+    }
+}
+
+autoRefresh()
+
+const cardsList = document.getElementById('table-Body');
+const searchBar = document.getElementById('searchBar');
+let hpcards = [];
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+
+    const filteredcards = hpcards.filter((card) => {
+        return (
+            card.manufacturer.toLowerCase().includes(searchString) ||
+            card.model.toLowerCase().includes(searchString)
+        );
+    });
+    displaycards(filteredcards);
+});
+
+const loadcards = async () => {
+    try {
+        const res = await fetch('http://localhost:3000/data');
+        hpcards = await res.json();
+        displaycards(hpcards);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const displaycards = (cards) => {
+    const htmlString = cards
+        .map((card) => {
+            return `
+             <tr class="cards">
+                   <td> <img src="${card.image}"></img></td>
+                    <td>${card.manufacturer}</td>
+                    <td>${card.model}</td>
+                    <td>${card.price}</td>
+                </tr>
+        `;
+        })
+        .join('');
+    cardsList.innerHTML = htmlString;
+};
+
+loadcards();
+
+
+const table = document.querySelector("#table-Body");
+const lastRow = table.rows[ table.rows.length - 1 ];
+
+
+
+
